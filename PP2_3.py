5c3efc7e321b678b0296965ae8360d463a587991
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-10-16 14:16:14 +0000

    abc

--- PP2_3.py
+++ PP2_3.py
@@ -9,20 +9,33 @@
 
 def q1(): 
   word = input("In: ")
-  if word [-1] == "y":
-    print("-ies")
-  if word [-2:] == "e":
+  if word [-2:] == "ey":
     print("-eys")
-  if word [-3:] == "ife":
+  elif word [-1] == "y":
+    print("-ies")
+  elif word [-3:] == "ife":
     print("-ives")
+  else:
+    print("-s")
 
 def q2(): 
-  print("e") 
+  num = int(input("In: "))
+  if num > 0:
+    print(f"{num} is positive")
+  if num < 0: 
+    print(f"{num} is negative")
 
-
+def q3():
+  num1 = float(input("Input a number: "))
+  num2 = float(input("Input a number: "))
+  num3 = float(input("Input a number: "))
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
+'''
 q1()
 q2()
+q3()
+
+'''
