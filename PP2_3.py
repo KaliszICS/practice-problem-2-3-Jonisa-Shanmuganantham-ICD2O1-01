d9ab3222583b62f6c27a5eb9512bb3a721319a35
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-10-22 02:57:15 +0000

    yipee

--- PP2_3.py
+++ PP2_3.py
@@ -29,6 +29,14 @@ def q3():
   num1 = float(input("Input a number: "))
   num2 = float(input("Input a number: "))
   num3 = float(input("Input a number: "))
+  if num1 + num2 <= num3 or num1 + num3 <= num2 or num2 + num3 <= num1:
+        print("No Triangle")
+  elif num1 == num2 == num3:
+        print("Equilateral")
+  elif num1 == num2 or num2 == num3 or num1 == num3:
+        print("Isosceles")
+  else:
+        print("Scalene")
 
 #Do not alter the following code
 #Comment out the following code when running your tests
